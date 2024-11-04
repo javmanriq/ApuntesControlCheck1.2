86fe8d06978723ce3e75ff442106dd7889e80a09
Javier Manrique Riquelme <Javier.Manrique688@gmail.com>
2024-11-04 20:38:28 +0100

    Test1 (Entidad)

--- src/main/java/org/springframework/samples/petclinic/insurance/Insurance.java
+++ src/main/java/org/springframework/samples/petclinic/insurance/Insurance.java
@@ -2,11 +2,33 @@ package org.springframework.samples.petclinic.insurance;
 
 import java.util.List;
 
+import org.springframework.samples.petclinic.model.BaseEntity;
 import org.springframework.samples.petclinic.pet.Pet;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Transient;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
-public class Insurance {
+@Entity
+@Getter
+@Setter
+public class Insurance extends BaseEntity {
+    
+    @NotNull
+    @Size(min=3,max=50)
+    @Column(unique = true)
     String name;
+    
+    @NotNull
+    @Min(0)
     Double price;
+    
+
+    @Transient
     List<Pet> pets;
 }
--- src/main/java/org/springframework/samples/petclinic/insurance/InsuranceRepository.java
+++ src/main/java/org/springframework/samples/petclinic/insurance/InsuranceRepository.java
@@ -3,8 +3,10 @@ package org.springframework.samples.petclinic.insurance;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.repository.CrudRepository;
 
-public interface InsuranceRepository {
+
+public interface InsuranceRepository extends CrudRepository<Insurance, Integer> {
     Insurance save(Insurance i);
     List<Insurance> findAll();
     Optional<Insurance> findById(Integer id);
